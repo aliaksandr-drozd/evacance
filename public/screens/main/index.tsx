cf2ee140915e01ca5767b7d6e93229d27d08be22
Cooler <itahmea@gmail.com>
2022-03-05 23:51:49 +0100

    remove geolocation request from main

--- public/screens/main/index.tsx
+++ public/screens/main/index.tsx
@@ -2,9 +2,8 @@ import React, { FC } from 'react'
 import { Button, Divider, Space } from 'antd-mobile'
 import { useNavigate } from 'react-router'
 import { useTranslation } from 'react-i18next'
-import useGeolocation from 'react-hook-geolocation'
 
-import { ICanHelpIcon, LanguageSwitch, TelegramIcon } from '../../common/components'
+import { LanguageSwitch, TelegramIcon } from '../../common/components'
 import { useLocalStorage } from '../../common/hooks'
 import { HELP_LOCALSTORAGE_KEY } from '../../common/consts'
 import { MyRequests } from './components'
@@ -14,8 +13,6 @@ export const MainScreen: FC = () => {
   const [, setIsHelpVisible] = useLocalStorage(HELP_LOCALSTORAGE_KEY, false)
   const navigate = useNavigate()
   const { t } = useTranslation()
-  useGeolocation()
-
 
   return (
     <>
