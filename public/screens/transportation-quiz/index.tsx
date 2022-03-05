95fce5fb25810e1fcbd40363fdb21544eeed441d
Cooler <itahmea@gmail.com>
2022-03-05 17:45:59 +0100

    transportation quiz screen

--- public/screens/transportation-quiz/index.tsx
+++ public/screens/transportation-quiz/index.tsx
@@ -1,36 +1,62 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { useNavigate } from 'react-router'
-import { NavBar, Slider } from 'antd-mobile'
+import { Button, Card, Divider, NavBar, Slider } from 'antd-mobile'
 import { useTranslation } from 'react-i18next'
 import useGeolocation from 'react-hook-geolocation'
 
 
 export const TransportationQuizScreen: FC = () => {
+  const [radius, setRadius] = useState(50)
   const { t } = useTranslation()
   const navigate = useNavigate()
   const geolocation = useGeolocation()
-
-  debugger
-
 
   return (
     <>
       <NavBar onBack={ () => navigate('/') } />
 
       {
-        geolocation.latitude !== null && geolocation.latitude && <>
-          Покажите всех, кого нужно отвезти, в радиусе 10 км:
-          <Slider
-            min={ 10 }
-            max={ 200 }
-            /*defaultValue={ tolerance }*/
-            // @ts-ignore
-            /*onAfterChange={ onToleranceChange }*/
-          />
+        geolocation.latitude !== null && geolocation.latitude &&
+        <>
+          <Card>
+            <h3>Покажите всех, кого нужно отвезти, в радиусе { radius } км от меня:</h3>
+            <Slider
+              min={ 20 }
+              max={ 500 }
+              defaultValue={ radius }
+              onAfterChange={ (v) => setRadius(v as number) }
+            />
+            <br />
+            <br />
+            <Button
+              color="primary"
+              block
+            >
+              Показать тех, кто рядом
+            </Button>
+          </Card>
+          <Divider />
         </>
       }
 
+      <Card>
+        <Button
+          block
+          color="primary"
+        >
+          Покажите где нужна помощь на карте
+        </Button>
 
+        <Divider />
+
+        <Button
+          block
+          color="primary"
+          onClick={ () => navigate('/transportation') }
+        >
+          Я готов помочь тем, кому по пути со мной
+        </Button>
+      </Card>
 
 
     </>
