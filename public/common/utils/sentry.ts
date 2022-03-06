30f6f4d0e50770b91c56a30fe833d35a98fd35fc
Alex Rozum <rozumalex@gmail.com>
2022-03-06 10:02:20 +0100

    feat: run sentry on production only

--- public/common/utils/sentry.ts
+++ public/common/utils/sentry.ts
@@ -1,14 +1,14 @@
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
-// if (process.env.NODE_ENV === 'production')
-Sentry.init({
-  dsn: process.env.FRONTEND_SENTRY_DSN,
-  integrations: [new BrowserTracing()],
+if (process.env.NODE_ENV === 'production')
+  Sentry.init({
+    dsn: process.env.FRONTEND_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: process.env.FRONTEND_SENTRY_TRACE_SAMPLE_RATE,
-  environment: process.env.NODE_ENV,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: process.env.FRONTEND_SENTRY_TRACE_SAMPLE_RATE,
+    environment: process.env.NODE_ENV,
+  });
